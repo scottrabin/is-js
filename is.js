688a492232c1835d63ceefbfc483c04ad381ed0f
Scott Rabin <scottrabin@gmail.com>
2012-08-18 13:50:27 -0700

    Added `is.in` for testing included values

--- is.js
+++ is.js
@@ -8,9 +8,25 @@
 }(this, function() {
 
 	// Local references to global functions (better minification)
-	var toString = Object.prototype.toString,
-		hasOwn     = Object.prototype.hasOwnProperty,
-		ArrayProto = Array.prototype;
+	var ObjProto   = Object.prototype,
+		ArrayProto = Array.prototype,
+		toString   = ObjProto.toString,
+		hasOwn     = ObjProto.hasOwnProperty,
+
+		// these get used internally, so define it in scope and then expose it
+		is_array   = ArrayProto.isArray || function(a) { return toString.call(a) === '[object Array]'; },
+		is_object  = function(o) { return o === Object(o); },
+		// in case the browser doesn't have it
+		index_of   = ArrayProto.indexOf ?
+			function(arr, val) { return arr.indexOf(val); } :
+			function(arr, val) {
+				for(var i = 0, l = arr.length ; i < l ; i++) {
+					if (arr[i] === val) {
+						return i;
+					}
+				}
+				return -1;
+			};
 
 	return {
 		// Primitives - test for type exactness
@@ -28,14 +44,10 @@
 		fn     : function(f) {
 			return toString.call(f) === '[object Function]';
 		},
-		// array - delegate to builtin if available
-		array  : ArrayProto.isArray || function(a) {
-			return toString.call(a) === '[object Array]';
-		},
+		// array - delegates to builtin if available
+		array  : is_array,
 		// basically all Javascript types are objects
-		object : function(o) {
-			return o === Object(o);
-		},
+		object : is_object,
 
 		// HTML elements
 		// https://github.com/documentcloud/underscore/blob/master/underscore.js#L836-838
@@ -71,6 +83,13 @@
 			// indicate the presence of any prototype properties
 			for (var key in o){}
 			return (key === undefined || hasOwn.call(o, key));
+		},
+
+		// test for containment, in both arrays and objects
+		"in" : function(container, val) {
+			return (is_array(container) ? index_of(container, val) > -1 :
+					is_object(container) ? hasOwn.call(container, val) :
+					false);
 		}
 	};
 }));
