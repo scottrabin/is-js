ff12151989212e74492a0e2eb85d9c0ff658a451
Scott Rabin <scottrabin@gmail.com>
2012-08-18 13:18:05 -0700

    Added `is.hash` to test for key/value-type objects

--- is.js
+++ is.js
@@ -9,6 +9,7 @@
 
 	// Local references to global functions (better minification)
 	var toString = Object.prototype.toString,
+		hasOwn     = Object.prototype.hasOwnProperty,
 		ArrayProto = Array.prototype;
 
 	return {
@@ -46,6 +47,30 @@
 		// http://dl.dropbox.com/u/35146/js/tests/isNumber.html
 		numeric : function(n) {
 			return !isNaN(parseFloat(n)) && isFinite(n);
+		},
+
+		// plain objects - not a specific type, just an object with key/value pairs
+		// https://github.com/jquery/jquery/blob/c14a6b385fa419ce67f115e853fb4a89d8bd8fad/src/core.js#L425-452
+		hash : function(o) {
+			// fail fast for falsy/non-object/HTMLElement/window objects
+			if (!o || typeof o !== 'object' || o.nodeType || o === window) {
+				return false;
+			}
+
+			// check constructor properties - objects don't have their own constructor,
+			// and their constructor does not have its own `isPrototypeOf` function
+			if (o.constructor &&
+				! hasOwn.call(o, 'constructor') &&
+				! hasOwn.call(o.constructor.prototype, 'isPrototypeOf')
+			) {
+				return false;
+			}
+
+			// from jQuery source: speed up the test by cycling to the last property,
+			// since own properties are iterated over first and therefore the last one will
+			// indicate the presence of any prototype properties
+			for (var key in o){}
+			return (key === undefined || hasOwn.call(o, key));
 		}
 	};
 }));
