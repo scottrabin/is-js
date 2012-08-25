b11062e3e5b0e92ce17355997857c8754c7bcb2d
Scott Rabin <scottrabin@gmail.com>
2012-08-25 10:49:28 -0700

    Refactored internal structure for consistency
    Brought about by the need for `is.empty` to be recursive when inspecting
    objects that have a non-object `valueOf()`. Fix #1 (`is.string` &
    similar fail for objects created via `new [Primitive]()`)

--- is.js
+++ is.js
@@ -12,18 +12,8 @@
 		ArrayProto = Array.prototype,
 		toString   = ObjProto.toString,
 		hasOwn     = ObjProto.hasOwnProperty,
-
-		// these get used internally, so define it in scope and then expose it
-		is_array   = ArrayProto.isArray || function(a) { return toString.call(a) === '[object Array]'; },
-		is_object  = function(o) { return o === Object(o); },
-
-		is_element = (typeof HTMLElement !== 'undefined' ?
-					  function(e) { return (e instanceof HTMLElement); } :
-					  // https://github.com/documentcloud/underscore/blob/master/underscore.js#L836-838
-					  function(e) { return !!(e && e.nodeType === 1); }
-					 );
 		// in case the browser doesn't have it
-		index_of   = ArrayProto.indexOf ?
+		index_of   = (ArrayProto.indexOf ?
 			function(arr, val) { return arr.indexOf(val); } :
 			function(arr, val) {
 				for(var i = 0, l = arr.length ; i < l ; i++) {
@@ -32,47 +22,40 @@
 					}
 				}
 				return -1;
-			};
+			}),
+		// define the functions in the local scope to enable internal re-use
 
-	return {
 		// Primitives - test for type exactness
 		// typeof is faster; see http://jsperf.com/underscore-js-istype-alternatives/7
-		string : function(s) {
-			return (typeof s === 'string');
-		},
-		number : function(n) {
-			return (typeof n === 'number');
-		},
-		bool   : function(b) {
-			return b === !!b;
-		},
+		// typeof does not catch values made with a constructor, but is still faster than to String
+		//   see : http://jsperf.com/is-js
+		is_string  = function(s) { return (typeof s === 'string') || s instanceof String; },
+		is_number  = function(n) { return (typeof n === 'number') || n instanceof Number; },
+		is_bool    = function(b) { return b === !!b || b instanceof Boolean; },
 
 		// non-primitive builtin types
-		fn     : function(f) {
-			return (typeof f === 'function');
-		},
+		is_fn      = function(f) { return (typeof f === 'function'); },
 		// array - delegates to builtin if available
-		array  : is_array,
+		is_array   = ArrayProto.isArray || function(a) { return toString.call(a) === '[object Array]'; },
 		// basically all Javascript types are objects
-		object : is_object,
-
+		is_object  = function(o) { return o === Object(o); },
 		// duck typing, because there isn't really a good way to do this
-		regex  : function(r) {
-			return !!(r && r.test && r.exec && (r.ignoreCase || r.ignoreCase === false));
-		},
+		is_regex   = function(r) { return !!(r && r.test && r.exec && (r.ignoreCase || r.ignoreCase === false)); },
 
 		// HTML elements
-		element : is_element,
+		is_element = (typeof HTMLElement !== 'undefined' ?
+					  function(e) { return (e instanceof HTMLElement); } :
+					  // https://github.com/documentcloud/underscore/blob/master/underscore.js#L836-838
+					  function(e) { return !!(e && e.nodeType === 1); }
+					 ),
 
 		// non-strict type checking
 		// http://dl.dropbox.com/u/35146/js/tests/isNumber.html
-		numeric : function(n) {
-			return !isNaN(parseFloat(n)) && isFinite(n);
-		},
+		is_numeric = function(n) { return !isNaN(parseFloat(n)) && isFinite(n); },
 
 		// plain objects - not a specific type, just an object with key/value pairs
 		// https://github.com/jquery/jquery/blob/c14a6b385fa419ce67f115e853fb4a89d8bd8fad/src/core.js#L425-452
-		hash : function(o) {
+		is_hash    = function(o) {
 			// fail fast for falsy/non-object/HTMLElement/window objects
 			// also check constructor properties - objects don't have their own constructor,
 			// and their constructor does not have its own `isPrototypeOf` function
@@ -90,7 +73,7 @@
 		},
 
 		// test for containment, in both arrays and objects
-		inside : function(container, val) {
+		is_inside  = function(container, val) {
 			if (is_array(container)) {
 				return index_of(container, val) > -1;
 			} else if (is_object(container)) {
@@ -106,15 +89,18 @@
 		},
 
 		// test for variable being undefined or null
-		set   : function(v) {
-			return v !== null && v !== (void 0);
-		},
+		is_set     = function(v) { return v !== null && v !== (void 0); },
 
 		// test for having any elements (if an array), any properties (if an object), or falsy-ness
-		empty : function(container) {
+		is_empty   = function(container) {
 			if (is_array(container)) {
 				return container.length === 0;
 			} else if (is_object(container)) {
+				// when an object has a valueOf function that doesn't return an object,
+				// object is empty if value is empty
+				if (is_fn(container.valueOf) && !is_object(container.valueOf())) {
+					return is_empty(container.valueOf());
+				}
 				for(var x in container) {
 					if (hasOwn.call(container, x)) {
 						return false;
@@ -124,6 +110,22 @@
 			} else {
 				return !container;
 			}
-		}
+		};
+
+	// expose the external functions
+	return {
+		string  : is_string,
+		number  : is_number,
+		bool    : is_bool,
+		fn      : is_fn,
+		array   : is_array,
+		object  : is_object,
+		regex   : is_regex,
+		element : is_element,
+		numeric : is_numeric,
+		hash    : is_hash,
+		inside  : is_inside,
+		set     : is_set,
+		empty   : is_empty
 	};
 }));
