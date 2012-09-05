e3e217ea43e2a961dda8b93b25cb4027bd9d0e66
Scott Rabin <scottrabin@gmail.com>
2012-09-04 23:25:07 -0700

    Better exports for Node.js

--- is.js
+++ is.js
@@ -8,6 +8,9 @@
 	if (typeof define === 'function' && define.amd) {
 		// AMD; don't export to window globals
 		define(factory);
+	} else if (typeof exports === 'object') {
+		// Node
+		module.exports = factory();
 	} else {
 		root.is = factory();
 	}
